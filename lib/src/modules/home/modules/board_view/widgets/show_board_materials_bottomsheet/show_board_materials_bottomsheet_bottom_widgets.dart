d10e9ef21aa377c9dea24e31b7c979ae81df9dae
nandohawkeye <luizfernando.m.neves@gmail.com>
2025-02-18 00:12:06 -0300

    Componetized some widgets about show board materials bottomsheet

---
+++ lib/src/modules/home/modules/board_view/widgets/show_board_materials_bottomsheet/show_board_materials_bottomsheet_bottom_widgets.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tormenta20/src/core/theme/t20_ui.dart';
+import 'package:tormenta20/src/core/theme/theme.dart';
+import 'package:tormenta20/src/modules/home/modules/board_view/widgets/show_board_materials_bottomsheet/show_board_materials_bottomsheet.dart';
+import 'package:tormenta20/src/modules/home/modules/board_view/widgets/show_board_materials_bottomsheet/show_board_materials_bottomsheet_store.dart';
+import 'package:tormenta20/src/modules/home/widgets/simple_button.dart';
+import 'package:tormenta20/src/shared/widgets/divider_level_two.dart';
+import 'package:tormenta20/src/shared/widgets/main_button.dart';
+import 'package:tormenta20/src/shared/widgets/simple_close_button.dart';
+
+class ShowBoardMaterialsBottomsheetBottomWidgets extends StatelessWidget {
+  const ShowBoardMaterialsBottomsheetBottomWidgets(
+    this.store, {
+    super.key,
+  });
+
+  final ShowBoardMaterialsBottomsheetStore store;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const DividerLevelTwo(verticalPadding: 0),
+        Padding(
+          padding: T20UI.allPadding,
+          child: Row(
+            children: [
+              Expanded(
+                child: AnimatedBuilder(
+                    animation: store,
+                    builder: (_, __) {
+                      final mode = store.mode;
+                      final toDelete = store.materialToDelete;
+
+                      return MainButton(
+                        label: mode == ShowBoardMaterialBottomSheetMode.add
+                            ? 'Adicionar'
+                            : 'Deletar',
+                        backgroundColor:
+                            (mode == ShowBoardMaterialBottomSheetMode.delete &&
+                                    toDelete.isEmpty)
+                                ? palette.backgroundLevelTwo
+                                : palette.selected,
+                        onTap: () {
+                          if (mode == ShowBoardMaterialBottomSheetMode.add) {
+                            store.addFiles();
+                          } else {
+                            if (toDelete.isNotEmpty) {
+                              store.deleteMaterials();
+                            }
+                          }
+                        },
+                      );
+                    }),
+              ),
+              T20UI.spaceWidth,
+              AnimatedBuilder(
+                animation: store,
+                builder: (_, __) {
+                  final mode = store.mode;
+                  return SimpleButton(
+                    icon: mode == ShowBoardMaterialBottomSheetMode.add
+                        ? FontAwesomeIcons.solidTrashCan
+                        : FontAwesomeIcons.plus,
+                    iconColor: palette.onSelected,
+                    backgroundColor: palette.selected,
+                    onTap: () {
+                      if (mode == ShowBoardMaterialBottomSheetMode.add) {
+                        store
+                            .chageMode(ShowBoardMaterialBottomSheetMode.delete);
+                      } else {
+                        store.chageMode(ShowBoardMaterialBottomSheetMode.add);
+                      }
+                    },
+                  );
+                },
+              ),
+              T20UI.spaceWidth,
+              SimpleCloseButton(
+                backgroundColor: palette.backgroundLevelTwo,
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/src/modules/home/modules/board_view/widgets/show_board_materials_bottomsheet/show_board_materials_bottomsheet_list.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:tormenta20/gen/assets.gen.dart';
+import 'package:tormenta20/src/core/theme/t20_ui.dart';
+import 'package:tormenta20/src/modules/home/modules/board_view/widgets/board_view_material_card.dart';
+import 'package:tormenta20/src/modules/home/modules/board_view/widgets/show_board_materials_bottomsheet/show_board_materials_bottomsheet_store.dart';
+import 'package:tormenta20/src/shared/widgets/screen_image_button.dart';
+
+class ShowBoardMaterialsBottomsheetList extends StatelessWidget {
+  const ShowBoardMaterialsBottomsheetList(
+    this.store, {
+    super.key,
+  });
+
+  final ShowBoardMaterialsBottomsheetStore store;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: store,
+      builder: (_, __) {
+        final materials = store.materials;
+
+        if (materials.isEmpty) {
+          return ScreenImageButton(
+            imageAsset: Assets.images.pdf.path,
+            borderRadius: BorderRadius.zero,
+            title: 'Materiais',
+            subtitle:
+                'Adicione arquivos de PDF e imagens para consultar rapidamente',
+            onTap: store.addFiles,
+          );
+        }
+
+        final mode = store.mode;
+        final toDelete = store.materialToDelete;
+
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            T20UI.spaceHeight,
+            SizedBox(
+              height: 120,
+              width: double.infinity,
+              child: ListView.separated(
+                shrinkWrap: true,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: T20UI.spaceSize - 4),
+                scrollDirection: Axis.horizontal,
+                itemCount: materials.length,
+                separatorBuilder: T20UI.separatorBuilderHorizontal,
+                itemBuilder: (_, index) {
+                  return BoardViewMaterialCard(
+                    material: materials[index],
+                    mode: mode,
+                    toDelete: toDelete,
+                    onDelete: store.addMaterialToDelete,
+                  );
+                },
+              ),
+            ),
+            T20UI.spaceHeight,
+          ],
+        );
+      },
+    );
+  }
+}
